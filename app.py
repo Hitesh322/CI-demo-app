72a9d1def1091805c518cecfef64b3f2a519caff
Hitesh Mahajan <mahajanhitesh127@gmail.com>
2026-06-04 09:32:59 +0000

    pushed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return " from CI Pipeline after commit changes . lets go devops engineer "
+    return " it's Ci Pipeline"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
